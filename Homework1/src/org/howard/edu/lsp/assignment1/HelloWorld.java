0995966410e1cfcd57757797321fde03c630fdd9
Miles Murray <41023365+Milpay1@users.noreply.github.com>
2025-08-25 20:43:14 -0400

    Proper Project set up

---
+++ Homework1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Name: Miles Murray
+ */
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
