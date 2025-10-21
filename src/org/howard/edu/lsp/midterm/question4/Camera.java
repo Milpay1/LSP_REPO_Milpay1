195025be9984718bfe86e485599f5654570a98b9
Miles Murray <41023365+Milpay1@users.noreply.github.com>
2025-10-20 20:20:05 -0400

    camera and theromostat added

--- src/org/howard/edu/lsp/midterm/question4/Camera.java
+++ src/org/howard/edu/lsp/midterm/question4/Camera.java
@@ -1,5 +1,46 @@
 package org.howard.edu.lsp.midterm.question4;
 
-public class Camera {
+public class Camera extends Device implements Networked, BatteryPowered {
+    private int batteryPercent;
+
+    public Camera(String id, String location) {
+        super(id, location);
+        this.batteryPercent = 100; // Assume new device starts with full battery
+    }
+
+    @Override
+    public void connect() {
+        setConnected(true);
+    }
+
+    @Override
+    public void disconnect() {
+        setConnected(false);
+    }
+
+    @Override
+    public boolean isConnected() {
+        return super.isConnected(); 
+    }
+
+    @Override
+    public int getBatteryPercent() {
+        return this.batteryPercent;
+    }
+
+    @Override
+    public void setBatteryPercent(int percent) {
+        if (percent < 0 || percent > 100) {
+            throw new IllegalArgumentException("Battery percent must be between 0 and 100");
+        }
+        this.batteryPercent = percent;
+    }
+
+    @Override public String getStatus() {
+    String connStatus = isConnected() ? "up" : "down";
+    return "Camera[id=" + getId() + ", loc=" + getLocation() +
+            ", conn=" + connStatus + ", batt=" + batteryPercent + "%]";
+    }
+
 
 }
--- src/org/howard/edu/lsp/midterm/question4/Thermostat.java
+++ src/org/howard/edu/lsp/midterm/question4/Thermostat.java
@@ -1,5 +1,53 @@
 package org.howard.edu.lsp.midterm.question4;
 
-public class Thermostat {
+public class Thermostat extends Device implements BatteryPowered, Networked {
+    private int batteryPercent;
+    private double temperatureC;
+
+    public Thermostat(String id, String location, double initialTempC) {
+        super(id, location);
+        this.batteryPercent = 100; // Default battery percent
+        this.temperatureC = initialTempC; // Default temperature
+    }
+
+    @Override
+    public int getBatteryPercent() {
+        return batteryPercent;
+    }
+
+    @Override
+    public void setBatteryPercent(int percent) {
+        if (percent < 0 || percent > 100) {
+            throw new IllegalArgumentException("Battery percent must be between 0 and 100");
+        }
+        this.batteryPercent = percent;
+    }
+
+    @Override
+    public void connect() {
+        setConnected(true);
+    }
+
+    @Override
+    public void disconnect() {
+        setConnected(false);
+    }
+
+    @Override
+    public String getStatus() {
+        return "Thermostat at " + getLocation() + " is " + (isConnected() ? "connected" : "disconnected") +
+               ", Battery: " + batteryPercent + "%, Temperature: " + temperatureC + "°F";
+    }
+
+    public double getTempC() {
+        return temperatureC;
+    }
+
+    public void setTempC(double temperature) {
+        this.temperatureC = temperature;
+    }
+
+    @Override public boolean isConnected() { return super.isConnected(); }
+    
 
 }
